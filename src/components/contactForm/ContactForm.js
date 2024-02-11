82559aea7a347ed45bbbcc44a51dca7ea19daf39
Serhiy Korytskyy <korysergey55@gmail.com>
2024-02-11 20:57:31 +0200

    patterns

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -35,7 +35,7 @@ class ContactForm extends Component {
             value={this.state.name}
             type="text"
             id="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. 
         For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
@@ -50,7 +50,7 @@ class ContactForm extends Component {
             value={this.state.number}
             type="tel"
             id="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
             onChange={this.handleChange}
